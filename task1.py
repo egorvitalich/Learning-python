eb0eea55423a148e243fc08ba7e425d3a4f36571
egorvitalich <egor.sirota2018@gmail.com>
2025-09-02 14:18:34 +0700

    1 день обучения

---
+++ task1.py
@@ -0,0 +1,7 @@
+#Задание: Посчитай площадь прямоугольника со сторонами 10 и 5. Проверь, больше ли она 40.
+length = 10
+height = 5
+square = length * height
+test = square > 40
+print(square)
+print(test)
